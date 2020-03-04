8a5c07eefc9d7576932fa4a0ff273432a37a26ad
Tarrowren <35626141+Tarrowren@users.noreply.github.com>
2020-03-04 17:44:49 +0800

    exports

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { Pixel } = require("./build/Release/node-win-pixel.node");
-exports.Pixel = Pixel;
+module.exports = require("./build/Release/node-win-pixel.node");
